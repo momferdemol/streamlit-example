473fe922ea91af0259710cccb9954f47e07ace19
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2024-09-04 13:32:57 +0200

    get parameter from store

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import altair as alt
 import numpy as np
 import pandas as pd
+import logging
 import streamlit as st
+from boto3 import client
+from botocore.expections import ClientError
+from mypy_boto3_ssm import SSMClient
 
 """
 # Welcome to Streamlit!
@@ -13,6 +17,18 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+
+def get_parameter_from_store(name: str) -> str:
+    try:
+        response = ssm.get_parameter(Name=name)
+        value = response["Parameter"]["Value"]
+        return value
+    except ClientError as err:
+        msg = f"{err}{name}"
+        logging.error(msg)
+        raise SystemError from err
+
+
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
@@ -23,18 +39,25 @@ radius = indices
 x = radius * np.cos(theta)
 y = radius * np.sin(theta)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+df = pd.DataFrame(
+    {
+        "x": x,
+        "y": y,
+        "idx": indices,
+        "rand": np.random.randn(num_points),
+    }
+)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
+st.altair_chart(
+    alt.Chart(df, height=700, width=700)
     .mark_point(filled=True)
     .encode(
         x=alt.X("x", axis=None),
         y=alt.Y("y", axis=None),
         color=alt.Color("idx", legend=None, scale=alt.Scale()),
         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    )
+)
+
+secret = get_parameter_from_store("/streamlit/app/survey/api-key")
+st.write(secret)
