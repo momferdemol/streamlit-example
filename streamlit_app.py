252784d5086943012fec10756a51b2cd1feeab29
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2024-09-04 16:00:12 +0200

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,32 +43,32 @@ st.write(f"FROM STORE: {secret}")
 logging.info("hello I am log?")
 
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# indices = np.linspace(0, 1, num_points)
+# theta = 2 * np.pi * num_turns * indices
+# radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# x = radius * np.cos(theta)
+# y = radius * np.sin(theta)
 
-df = pd.DataFrame(
-    {
-        "x": x,
-        "y": y,
-        "idx": indices,
-        "rand": np.random.randn(num_points),
-    }
-)
+# df = pd.DataFrame(
+#     {
+#         "x": x,
+#         "y": y,
+#         "idx": indices,
+#         "rand": np.random.randn(num_points),
+#     }
+# )
 
-st.altair_chart(
-    alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    )
-)
+# st.altair_chart(
+#     alt.Chart(df, height=700, width=700)
+#     .mark_point(filled=True)
+#     .encode(
+#         x=alt.X("x", axis=None),
+#         y=alt.Y("y", axis=None),
+#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
+#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+#     )
+# )
